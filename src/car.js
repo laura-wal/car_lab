f68d28ed33c3f3ea363914b971f3621df0ad1662
Laura Walpole <l.a.walpole@googlemail.com>
2015-04-30 17:21:41 +0100

    second question

--- src/car.js
+++ src/car.js
@@ -1,6 +1,15 @@
-function Car(make, model, year, color){
-  
+function Car(make, model, year, color, state){
+  this. year = year;
+  this.state = 'off';
 }
+
+
+//Car.prototype.state = function(state) {
+ // return (this.state);
+//}
+
+//var off = new Car ('off');
+
 
 Car.prototype.sale = function(newOwner){
 
